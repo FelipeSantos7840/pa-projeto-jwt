0f8ac3baa436b0405f118b510cf7890fe0f51478
Felipe dos Santos <felipe.santosmf2022@gmail.com>
2025-06-12 19:41:50 -0300

    Inicialização do Projeto

---
+++ src/test/java/com/felipesantos/projetosecurity/ProjetosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesantos.projetosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
