de7997b8b2cb7bc8f8f209fb55f45774575af147
Felipe dos Santos <felipe.santosmf2022@gmail.com>
2025-06-12 20:05:06 -0300

    Implementado service e repository

---
+++ src/main/java/com/felipesantos/projetosecurity/service/UserService.java
@@ -0,0 +1,23 @@
+package com.felipesantos.projetosecurity.service;
+
+import com.felipesantos.projetosecurity.model.User;
+import com.felipesantos.projetosecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user != null) return user;
+        else throw new UsernameNotFoundException("Username "+username+" not found!");
+    }
+}
