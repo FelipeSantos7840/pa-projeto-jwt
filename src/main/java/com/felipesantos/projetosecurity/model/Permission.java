d5622c3dfe4ea5bfacc62ec6b83a7a3749a3e0c9
Felipe dos Santos <felipe.santosmf2022@gmail.com>
2025-06-12 19:57:16 -0300

    Adicionado dependencias e implementado model

---
+++ src/main/java/com/felipesantos/projetosecurity/model/Permission.java
@@ -0,0 +1,41 @@
+package com.felipesantos.projetosecurity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+public class Permission implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public Permission() {
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/felipesantos/projetosecurity/model/User.java
@@ -0,0 +1,98 @@
+package com.felipesantos.projetosecurity.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(name = "fullname")
+    private String fullName;
+
+    @Column
+    private String password;
+
+
+    private Boolean accountNonExpired;
+
+    private Boolean accountNonLocked;
+
+    private Boolean credentialsNonExpired;
+
+    private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_permission",
+        joinColumns = {@JoinColumn (name = "id_user")},
+            inverseJoinColumns = {@JoinColumn(name = "id_permission")}
+    )
+    private List<Permission> permissions;
+
+    public User() {
+    }
+
+    public List<String> getRoles(){
+        List<String> roles = new ArrayList<>();
+        for (Permission permission: permissions){
+            roles.add(permission.getDescription());
+        }
+        return roles;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.permissions;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+}
