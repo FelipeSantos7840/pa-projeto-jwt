ce2b65e59383ad34f123f6568626d854696cb0ae
Felipe dos Santos <felipe.santosmf2022@gmail.com>
2025-06-12 21:49:29 -0300

    Gerado classes de Controller

---
+++ src/main/java/com/felipesantos/projetosecurity/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.felipesantos.projetosecurity.controller;
+
+import com.felipesantos.projetosecurity.dto.UserDTO;
+import com.felipesantos.projetosecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/create")
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO dto){
+
+    }
+}
