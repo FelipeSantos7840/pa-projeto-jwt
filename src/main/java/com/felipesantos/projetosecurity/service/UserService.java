62e0998674ffb1ac535af83308ef57d0840490dc
Felipe dos Santos <felipe.santosmf2022@gmail.com>
2025-06-12 21:48:31 -0300

    Implementado função de create

--- src/main/java/com/felipesantos/projetosecurity/service/UserService.java
+++ src/main/java/com/felipesantos/projetosecurity/service/UserService.java
@@ -1,11 +1,13 @@
 package com.felipesantos.projetosecurity.service;
 
+import com.felipesantos.projetosecurity.dto.UserDTO;
 import com.felipesantos.projetosecurity.model.User;
 import com.felipesantos.projetosecurity.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,11 +15,23 @@ public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user != null) return user;
         else throw new UsernameNotFoundException("Username "+username+" not found!");
+    }
+
+    public UserDTO create(UserDTO dto){
+        User user = new User();
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setUsername(dto.getUsername());
+        user.setRole(dto.getRole());
+
+        userRepository.save(user);
+        return dto;
     }
 }
