a912add408e8d978b6fbf5089ae7504f06e988e7
Felipe dos Santos <felipe.santosmf2022@gmail.com>
2025-06-12 21:55:17 -0300

    Criado novo URL

--- src/main/java/com/felipesantos/projetosecurity/controller/UserController.java
+++ src/main/java/com/felipesantos/projetosecurity/controller/UserController.java
@@ -4,19 +4,22 @@ import com.felipesantos.projetosecurity.dto.UserDTO;
 import com.felipesantos.projetosecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok(userService.findAll());
+    }
 
     @PostMapping("/create")
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO dto){
